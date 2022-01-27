b79e19e783e0a0686a7ea362609920cfcb877310
MakarandPundlik <mmp2308@gmail.com>
2022-01-27 20:32:33 +0530

    building frontend using jinja2

--- app.py
+++ app.py
@@ -1,11 +1,20 @@
 #flask entry poiny for index file
 from flask import Flask,render_template 
+import pandas as pd
 
 app = Flask(__name__)
 
+#create dataframe, read csv
+cars = pd.read_csv('Cleaned Car Data.csv')
+
+
 @app.route("/")
 def index():
-    return render_template('index.html')
+    companies = sorted(cars['company'].unique())
+    models = sorted(cars['name'].unique())
+    year = sorted(cars['year'].unique())
+    fuel_type = sorted(cars['fuel_type'].unique())
+    return render_template('index.html',companies=companies,models=models,years=year,fuel_type=fuel_type)
 
 if __name__=="__main__":
     app.run(debug=True)
